6503fed57ed6519ce72863dab57462cbd1ea1259
Hema Sama <mrlibya003@gmail.com>
2022-05-12 02:09:42 +0200

    + add types

---
+++ src/modules/DeviceInfo/index.d.ts
@@ -0,0 +1,7 @@
+
+declare type SystemDetails = {
+    [key: string]: string,
+};
+
+export declare function getSystemDetail(): () => Promise<SystemDetails>;
+export declare function getBattery(): () => Promise<number>;
